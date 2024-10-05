008fd75dfdbd75e8873012625ae0f4cae24bded9
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-05 20:06:44 -0300

    creating the basics for global data to transfer it between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import GlobalContext from './Data/GlobalContext';
 import Home from './pages/Home';
+import { Transaction } from './Types/Transaction';
 
 function App() {
   
+
+  const GlobalData = {
+    Finances: new Array<Transaction>,
+  }
+
   return (<>
-    
-    <main>
+  <pre>
+    {JSON.stringify(GlobalData, null, 2)}
+  </pre>
+    <GlobalContext.Provider value={GlobalContext}>
       <Home />
-    </main>
+    </GlobalContext.Provider>
     </>)
 }
 
