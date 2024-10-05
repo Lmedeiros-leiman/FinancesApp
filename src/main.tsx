d80414b23d6fc6b71062b3b2ac4797750b6c4bb6
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-05 20:31:12 -0300

    added the new styles to the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api'
 import App from './App.tsx'
 
-import"primereact/resources/themes/saga-blue/theme.css"
+import "primereact/resources/themes/saga-blue/theme.css"
+import "primeflex/primeflex.min.css"
+import "primeicons/primeicons.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
