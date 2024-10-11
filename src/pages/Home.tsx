5a41fd27a5b8dd327e81335f6525ea2ceddfd985
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-11 19:00:43 -0300

    re-organized main menu and made a navbar to hold the main data.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,28 +4,32 @@ import MonthlyGraph from "../components/MonthlyGraph";
 import TransactionHistory from "../components/TransactionHistory";
 import BalanceDisplay from "../components/BalanceDisplay";
 
-export default function Home() {
-   return (<>
-      <main>
-         
-         
-      <div className="flex flex-wrap justify-content-end ">
-      
-      
-      
-      <div className="flex flex-wrap justify-content-between bg-red-200 w-full  p-1 mb-2 ">
-         <Card className=" flex  mb-3 mt-4 ml-3">
-            <BalanceDisplay />
-            <InputTransaction/>
-         </Card>
-         <aside>
-            <MonthlyGraph/>
-         </aside>
-      </div>
 
+export default function Home() {
+   return (<main className="relative w-full">
+      
+      <nav className="mt-1 mb-2 mx-1 shadow-4 py-1 px-2 bg-white border-round">
+         <div className=" flex justify-content-between align-content-center align-items-center">
+            <span>
+               <BalanceDisplay />
+            </span>
+            <span>
+               <InputTransaction/>
+            </span>
          </div>
+      </nav>
+      
+      <div className="flex flex-wrap justify-content-center px-1">
+         <header className=" flex-grow-1 w-full">
+            <MonthlyGraph/>
+         </header>
          
-         <TransactionHistory/>
-      </main>
-   </>)
+         <footer className="text-center flex flex-column">
+            <h2>Saved Transaction</h2>
+            <TransactionHistory/>
+         </footer>
+      </div>
+         
+         
+      </main>)
 }
