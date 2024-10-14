58b957f877587f1806920dfce5802a0598e6b86b
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-13 22:18:24 -0300

    merged main and app into a single file

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api'
-import App from './App.tsx'
 
-import "primereact/resources/themes/saga-blue/theme.css"
+import { GlobalContext, useGlobalData } from './Data/GlobalContext.ts'
+import Sidebar from './layout/Sidebar.tsx'
+
+import "./styles/main.css"
 import "primeflex/primeflex.min.css"
 import "primeicons/primeicons.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PrimeReactProvider>
-      <App />
+      <App/>
     </PrimeReactProvider>
   </StrictMode>,
 )
+
+export function App() {
+  const {data, UpdateData} = useGlobalData();
+
+  (document.getElementById("mainStyle") as HTMLLinkElement).href  = `./node_modules/primereact/resources/themes/${ localStorage.getItem("theme") || "saga-blue" }/theme.css`;
+
+
+  return (<>
+    <GlobalContext.Provider value={{data, UpdateData}}  >
+      <Sidebar />
+    </GlobalContext.Provider>
+  </>)
+}
