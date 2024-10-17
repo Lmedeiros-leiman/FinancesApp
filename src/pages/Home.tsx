91746d559986cd5f3f0b570403aaac11c0af6e7c
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-17 17:56:08 -0300

    made elements follow theme colors.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import BalanceDisplay from "../components/BalanceDisplay";
 export default function Home() {
    return (<main className="relative w-full">
       
-      <nav className="mt-1 mb-2 mx-1 shadow-4 py-1 px-2 bg-white border-round">
+      <nav className="mt-1 mb-2 mx-1 mt-2 shadow-4 py-1 px-2 surface-ground border-round">
          <div className=" flex justify-content-between align-content-center align-items-center">
             <BalanceDisplay />
             <InputTransaction/>
@@ -20,7 +20,6 @@ export default function Home() {
          </header>
          
          <footer className="text-center flex mt-5 flex-column w-full">
-            
             <TransactionHistory/>
          </footer>
       </div>
