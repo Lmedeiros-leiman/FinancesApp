f2f3fc0545d7b499389f681a97ea4648c0d909e8
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-21 18:21:43 -0300

    Input transaction now handles the new currency input type.

--- src/components/InputTransaction.tsx
+++ src/components/InputTransaction.tsx
@@ -1,6 +1,6 @@
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Transaction } from "../Data/Types/Transaction";
 import { Calendar } from "primereact/calendar";
 import { Database, DatabaseStores } from "../Data/Database";
@@ -8,149 +8,156 @@ import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
 import { InputNumber } from "primereact/inputnumber";
 import { Dropdown } from "primereact/dropdown";
-import { IconList, TransactionType, TransactionTypes } from "../Data/Selections/TransactionTypes";
-import { GlobalDataContext, GlobalDataContextType } from "../Data/Contexts/GlobalDataContext";
+import { Currency, GlobalDataContext, GlobalDataContextType } from "../Data/Contexts/GlobalDataContext";
 import "../styles/InputTransaction.css";
 
 export default function InputTransaction() {
    const context = useContext(GlobalDataContext) as GlobalDataContextType
-   const [open, setOpen] = useState(false);
-   const [newTransaction, setNewTransaction] = useState<Transaction>({
+   const [defaultTransaction] = useState({
       title: "",
       amount: 0,
-      ammountType: "USD",
+      // user base currency or default USD.
+      ammountType: context.data.User.BaseCurrency || {
+         "code": "USD",
+         "name": "US Dollar",
+         "decimal_digits": 2,
+         "name_plural": "US dollars",
+         "rounding": 0,
+         "symbol": "$",
+         "symbol_native": "$"
+      },
       category: "",
-      dateTime: new Date(/* current date */).getTime(),
+      dateTime: new Date().getTime(),
       type: null,
       // these values get added once the transaction is saved.
-      createdAt: new Date(/* current date */).getTime(),
-      id: crypto.randomUUID(),
+      createdAt: new Date().getTime(),
+      id: crypto.randomUUID()
    });
 
+   const [open, setOpen] = useState(false);
+   const [newTransaction, setNewTransaction] = useState<Transaction>(defaultTransaction);
 
+   const [DropdownOptions, setDropDownOptions] = useState(Object.values(context.data.ValidCurrencies));
+   useEffect( () => {
+      setDropDownOptions(Object.values(context.data.ValidCurrencies))
+   },[context.data.ValidCurrencies]);
 
-   function HandleClose() {
-      setNewTransaction({
-         title: "",
-         amount: 0,
-         ammountType: "USD",
-         category: "",
-         dateTime: new Date(/* current date */).getTime(),
-         type: null,
-         // these values get added once the transaction is saved.
-         createdAt: new Date(/* current date */).getTime(),
-         id: crypto.randomUUID(),
-      })
+   const HandleClose = () => {
+      setNewTransaction(defaultTransaction)
       setOpen(false);
 
    }
-   async function HandleSave() {
+   const HandleSave = async (e : React.SyntheticEvent) => {
+      e.preventDefault()
       console.log("Adding new transaction to database...")
+
+      //
+      // do some data handling before inserting into the database.
+      setNewTransaction({
+         ...newTransaction, 
+         type: newTransaction.amount > 0 ? "Expense" : "Income",
+         createdAt: new Date().getTime(),
+      });
+      
+      //
+      // now we can insert it.
       const db = await Database.getDB();
-      
-      if (newTransaction.type == "Expense") {
-         newTransaction.amount *= -1
+      while (await db.getKey(DatabaseStores.Finances, newTransaction.id) == newTransaction.id) {
+         // making sure its ID is unique.
+         setNewTransaction({ ...newTransaction, id: crypto.randomUUID() })
       }
-
-      while (await db.getKey(DatabaseStores.Finances, newTransaction.id) == newTransaction.id ) {
-         setNewTransaction({...newTransaction, id : crypto.randomUUID()})
-      }
-
-      setNewTransaction({ ...newTransaction, createdAt: new Date().getTime() })
       await db.add(DatabaseStores.Finances, newTransaction)
-      
       context.UpdateData(prevData => ({
-         ...prevData, 
+         ...prevData,
          Finances: [
-            ...prevData.Finances, 
+            ...prevData.Finances,
             newTransaction
          ]
       }));
-      
-      
+
       console.log("New transaction added to database.")
-      await db.close()
+      db.close()
       HandleClose();
    }
-   return (<div className="InputTransaction">
-      <Button className="" onClick={() => setOpen(true)}>
-         <i className="pi pi-plus pr-3"></i> 
-         <span>Add new transaction</span>
+
+   return (<span>
+      <Button className="gap-1 "
+         onClick={() => setOpen(true)}
+         icon="pi pi-plus">
+         <span>Add Transaction</span>
       </Button>
-      
-      <Dialog header="New Transaction" visible={open} draggable={false} onHide={HandleClose}>
-         <form onSubmit={(e) => { e.preventDefault(); HandleSave() }}>
-         <div className="flex flex-column gap-2 pt-4 " >
-            <header className="mb-4 flex gap-3">
-               <FloatLabel >
-                  <InputText required id="Transactiontitle" value={newTransaction.title} onChange={(e) => setNewTransaction({ ...newTransaction, title: e.target.value })} ></InputText>
-                  <label htmlFor="Transactiontitle">Title</label>
-               </FloatLabel>
+
+      <Dialog header="Adding new transaction" visible={open} draggable={false}
+         onHide={HandleClose}>
+         <form onSubmit={HandleSave}
+            className="mt-4 flex flex-column gap-4">
+            <div>
                <FloatLabel>
-                  <Dropdown style={{ width: "180px" }}
-                     value={newTransaction.type} editable clearIcon
-                     options={TransactionTypes}
-                     optionLabel="name"
-                     optionValue="value"
-                     valueTemplate={(option : TransactionType) => {
-                        if (option) {
-                           return (<div className="flex align-items-center">
-                              <i className={`pi ${ IconList[option.name as keyof typeof IconList] } mr-2`}></i>
-                              <span>{option.name}</span>
-                           </div>)
-                        }
-                        return <span> </span>;
-                     }}
-                     itemTemplate={(option : TransactionType) => {
-                        return (<div className="flex align-items-center"> 
-                           <i className={`pi ${ IconList[option.name as keyof typeof IconList] } mr-2`}></i> 
-                           <span>{option.name}</span>
-                        </div>)
-                     }}
-                     onChange={(e) => { 
-                        setNewTransaction({ 
-                           ...newTransaction, 
-                           type: e.value
-                        })
-                     }}
-
-                     placeholder="Transaction Type"
+                  <InputText required id="title"
+                     value={newTransaction.title}
+                     onChange={(e) => setNewTransaction({ ...newTransaction, title: e.target.value })}
                   />
-                  <label>Transaction Type</label>
+                  <label>Title</label>
                </FloatLabel>
-            </header>
-            <FloatLabel className="mb-4">
-               <InputNumber required mode="currency" currency="USD" locale={navigator.language} id="Transactionamount" value={newTransaction.amount}
-                  onValueChange={(e) => {
-                     if (e.value != null && e.value != undefined) {
-                        setNewTransaction({ ...newTransaction, amount: e.value })
-                     }
-                  }} />
-               <label>Transaction Amount</label>
-            </FloatLabel>
-            <FloatLabel className="mb-4">
-               <Calendar touchUI={context.data.User.IsMobile}
-                  value={new Date(newTransaction.dateTime)} showTime hourFormat="12" required
-                  onChange={(e) => {
-                     if (e.value != null && e.value != undefined) {
-                        setNewTransaction({ ...newTransaction, dateTime: e.value.getTime() })
-                     }
+            </div>
+            <div className="flex gap-1">
+               <FloatLabel>
+               <Dropdown filter loading={context.data.FetchingCurrencies}
+                  filterBy="name" options={DropdownOptions}
+                  itemTemplate={(item) => {
+                     const currency = item as Currency
+                     return (<>
+                        <div className="flex gap-2">
+                           <aside className="p-1 w-2rem flex justify-content-center  border-round-3xl">
+                              {currency.symbol_native || currency.symbol}
+                           </aside>
+                           <span>{currency.name}</span>
+                           <div>
+                              {currency.code}
+                           </div>
+                        </div>
+                     </>);
                   }}
+                  onChange={(e) => {
+                     if (e.value === null) return;
+                     setNewTransaction({ ...newTransaction, ammountType: e.value })
+                  }}
+                  value={newTransaction.ammountType}
+                  valueTemplate={(item) => {
+                     const currency = item as Currency
+                     return(<> {currency.symbol_native || currency.symbol} </>)
+                  }} />
+                  <label>Currency</label>
+                  </FloatLabel>
+               <FloatLabel>
+                  <InputNumber required id="amount" maxFractionDigits={6}
+                     value={newTransaction.amount}
+                     onChange={(e) => setNewTransaction({ ...newTransaction, amount: e.value ? e.value : 0 })}
+                  />
+                  <label>Transaction Ammount</label>
+               </FloatLabel>
+            </div>
+            <div>
+               <Calendar id="TransactionDateTime" 
+                  touchUI={context.data.User.IsMobile} showIcon showButtonBar
+                  value={ new Date(newTransaction.dateTime)} showTime hourFormat="12"
+                  onChange={(e) => setNewTransaction({ ...newTransaction, dateTime: e.value?.getTime() || 0 })}
                />
-               <label>Transaction date</label>
-            </FloatLabel>
-            <FloatLabel>
-               <InputText required id="Transactioncategory" value={newTransaction.category} onChange={(e) => setNewTransaction({ ...newTransaction, category: e.target.value })} ></InputText>
-               <label htmlFor="Transactioncategory">Categories</label>
-            </FloatLabel>
-
-         </div>
-         <footer className="mt-4 flex justify-content-end gap-2">
-            <Button outlined severity="danger" label="Cancel" icon="pi pi-times" type="reset"  onClick={HandleClose} />
-            <Button label="Save" severity="success" icon="pi pi-check" type="submit" />
-         </footer>
+            </div>
+            <div>
+               <FloatLabel>
+                  <InputText required id="category"
+                     value={newTransaction.category}
+                     onChange={(e) => setNewTransaction({ ...newTransaction, category: e.target.value })}
+                     />
+                     <label>Category</label>
+               </FloatLabel>
+            </div>
+            <footer className="flex justify-content-end gap-2">
+               <Button outlined severity="danger" type="button" label="Cancel" onClick={HandleClose} />
+               <Button severity="info" type="submit" label="Save" />
+            </footer>
          </form>
       </Dialog>
-   </div>)
-
+   </span>);
 }
