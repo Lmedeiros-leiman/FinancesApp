cfef125ee78fc61b7897b637e6de56d15a6f8f7c
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-21 16:30:19 -0300

    remade balance display to incorporate new user currency.

--- src/components/BalanceDisplay.tsx
+++ src/components/BalanceDisplay.tsx
@@ -2,45 +2,70 @@ import { useContext, useEffect, useState } from "react"
 import { InputText } from "primereact/inputtext"
 import { Button } from "primereact/button"
 import { Skeleton } from "primereact/skeleton"
-import { GlobalDataContext, GlobalDataContextType } from "../Data/Contexts/GlobalDataContext"
+import { Currency, GlobalDataContext, GlobalDataContextType } from "../Data/Contexts/GlobalDataContext"
 
 
 
 export default function BalanceDisplay() {
-   return(<>BEING REWORKED.</>)
    const context = useContext(GlobalDataContext) as GlobalDataContextType
-   const formater = new Intl.NumberFormat( navigator.language , {
-      currency: "USD",
-      style: "currency"
-   });
-   
-   const UserCurrency = context.data.User.BaseCurrency
 
-   const [hidden, setHidden] = useState(false)
-   const [savedValue, setSavedValue] = useState( "Loading..." )
-   
+   const [calculatedTotal, setCalculatedTotal] = useState(0);
+   const [showingData, setShowingData] = useState(true);
 
-   useEffect( () => {
-      // gets the balance until today and shows it.
+   const UserCurrency = context.data.User.BaseCurrency as Currency;
 
-      const value = data.data.Finances.reduce((prevData, currData) => { prevData += currData.amount; return prevData }, 0)
-      setSavedValue(formater.format(value))
-   }, [data.data.Finances]);
+   useEffect(() => {
+      // calculates the user current balance.
+      const calculateTotal = () => {
+         if (context.data.FetchingFinanceData || (context.data.FetchingExchangeData)) { return; }
+         if (context.data.User.BaseCurrency === undefined) { return; }
+         //
+         const ExchangeRates = context.data.Exchange;
+
+         const value = context.data.Finances.reduce((prevData, currData) => {
+            // if the transaction happened INBETWEEN the selected timeframe limit.
+            // for now its just if it happened before today's timestamp.
+            if (currData.dateTime < new Date().getTime()) {
+               // check if the exchange rates are available.
+               if (ExchangeRates?.rates != undefined) {
+                  // check if the currency is the same as the user base currency.
+                  if (currData.ammountType.code != UserCurrency.code) {
+                     currData.amount = ExchangeRates.rates[currData.ammountType.code] * currData.amount
+                  }
+               }
+
+               // we add the ammount even if its not the same as the user base currency.
+               // probably should turn that into an user config option.
+               prevData += currData.amount;
+            }
+            return prevData
+         }, 0);
+
+         setCalculatedTotal(value);
+      }
+
+      calculateTotal();
+
+   }, [context.data.Finances, context.data.Exchange, context.data.User.BaseCurrency]);
 
 
-   if (data.data.FetchingFinanceData) {
-      return (
+   if (context.data.FetchingFinanceData) {
+      return (<>
+         <span className="flex gap-1 align-items-center ">
+            <Skeleton width="2rem" height="3rem" />
+            <InputText value={"Loading..."} readOnly />
+            <Skeleton width="3rem" height="3rem" />
+         </span>
+      </>)
+   }
+
+
+   return (<>
       <span className="flex gap-1">
-         <Skeleton className="w-10rem h-2rem"/>
-         <Skeleton className="w-2rem h-2rem"/>
-      </span>)
-   } 
-
-   return (<span className="flex gap-1">
-         <InputText className="text-1xl" value={ !hidden && savedValue != "Loading..." ? "Hidden." : savedValue} readOnly /> 
-         
-         <Button onClick={() => setHidden(!hidden)}>
-            <i className={hidden ? "pi pi-eye" : "pi pi-eye-slash"} />
-         </Button> 
-   </span>)
+         <Button outlined className="m-0 px-2 py-1" icon=" "> { context.data.User.BaseCurrency?.symbol_native } </Button>
+         <InputText readOnly value={String(calculatedTotal)} />
+         <Button className="m-0 px-2 py-1" icon={ showingData ? "pi pi-eye-slash" : "pi pi-eye" } 
+         onClick={() => setShowingData(!showingData)} />
+      </span>
+   </>);
 }
