9ade7803241b92b03709f3472f6cb6378d17bbfe
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-21 21:24:11 -0300

    sidebar now doesnt overlap with stuff on mobile view.

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -14,11 +14,14 @@ const Pages: { [key: string]: JSX.Element } = {
 export default function Sidebar() {
    const [selectedPage, setSelectedPage] = useState<string>("Home")
 
+   // <aside className=" sm:sticky fixed sm:h-screen sm:w-fit w-screen z-5  bottom-0 left-0 surface-ground shadow-2 " >
 
    return (<div className="flex flex-grow-1 ">
       
-      <aside className=" sm:sticky fixed sm:h-screen sm:w-fit w-screen z-5 sm:top-0 top-auto left-0 surface-ground shadow-2 " >
-         <nav className="flex sm:flex-column  justify-content-between h-full py-2 px-1 ">
+      <aside className=" sm:w-fit sm:h-screen sm:sticky
+            fixed z-5 bottom-0 sm:bottom-auto sm:top-0 left-0 surface-ground shadow-3 w-screen" >
+         
+         <nav className="flex sm:flex-column justify-content-between h-full py-2 px-1 ">
             
             <span className="flex flex-column gap-1">
                <Button  icon="pi pi-home" onClick={() => setSelectedPage("Home")} />
