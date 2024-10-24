7a3310911ca78d2188016ca37820c170c9554837
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-24 14:49:58 -0300

    base currency fetch logic change

--- src/Data/Contexts/GlobalDataContext.tsx
+++ src/Data/Contexts/GlobalDataContext.tsx
@@ -6,7 +6,7 @@ import CacheStorage from "../Cache/CacheStorage";
 
 export const GlobalDataContext = createContext<GlobalDataContextType | null>(null);
 
-const GlobalDataProvider : React.FC<{children: React.ReactNode}> = ({ children }) => {
+const GlobalDataProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
    const [data, ChangeData] = useState<IGlobalData>({
       Finances: [],
@@ -15,7 +15,7 @@ const GlobalDataProvider : React.FC<{children: React.ReactNode}> = ({ children }
       FetchingExchangeData: true,
       ValidCurrencies: {},
       FetchingCurrencies: true,
-      User:{
+      User: {
          IsMobile: true,
          BaseCurrency: undefined,
       }
@@ -25,14 +25,36 @@ const GlobalDataProvider : React.FC<{children: React.ReactNode}> = ({ children }
    //
    // Helper functions
 
-   // fetches exchange data from the api or localstorage.
+   // fetches user base currency.
+   // also fetches exchange data from the api or cache.
    const FetchCurenciesApiData = async () => {
-      const data = await MoneyConversionApi.getLatestRates();
+
+      const userLocale = navigator.language || "en-US";
+      let userCurrencyData = await CacheStorage.get("UserCurrency", `./locales/${userLocale}/currency.json`) as Currency | undefined;
+
+      if (!userCurrencyData) {
+         try {
+            // fetches data from the public folder.
+            console.log("Fetching user locale data from the web...");
+            userCurrencyData = await (await fetch(`./locales/${userLocale}/currency.json`)).json() as Currency;
+            CacheStorage.add("UserCurrency", `./locales/${userLocale}/currency.json`, userCurrencyData, 60 * 60 * 24 * 30);
+
+         } catch (error) {
+            console.log("Something went wrong.")
+            console.error(error)
+         }
+      }
+
+      const data = await MoneyConversionApi.getLatestRates((userCurrencyData as Currency).code);
 
       ChangeData((PrevData) => ({
          ...PrevData,
          Exchange: data,
          FetchingExchangeData: false,
+         User: {
+            ...PrevData.User,
+            BaseCurrency: userCurrencyData
+         }
       }));
    }
 
@@ -42,13 +64,13 @@ const GlobalDataProvider : React.FC<{children: React.ReactNode}> = ({ children }
          const db = await Database.getDB();
          console.log("Fetching finance data...");
          const financeData = await db.getAll(DatabaseStores.Finances);
- 
+
          ChangeData((PrevData) => ({
             ...PrevData,
             Finances: financeData,
             FetchingFinanceData: false,
          }));
- 
+
          db.close()
          console.log("Finance data loaded sucessfully.")
       } catch (err) {
@@ -68,69 +90,39 @@ const GlobalDataProvider : React.FC<{children: React.ReactNode}> = ({ children }
          ValidCurrencies: data,
          FetchingCurrencies: false,
       }));
-
    }
    const detectMobile = async () => {
 
-      if ( 
+      if (
          navigator.userAgent.match(/Windows/i) ||
          navigator.userAgent.match(/Linux/i) ||
          navigator.userAgent.match(/Mac OS/i)
-         ){
-            ChangeData((PrevData) => ({
-               ...PrevData,
-               User: {
-                  ...PrevData.User,
-                  IsMobile: false
-               }
-            }));
-         }
-   }
-   const DetectUserCurrency = async () => {
-      const userLocale = navigator.language || "en-US";
-      
-      let data = await CacheStorage.get("UserCurrency", `./locales/${userLocale}/currency.json`);
-      // we attempt to fetch the data from the cache first, if its not present we fetch it from the web.
-      if (!data) {
-         try {
-            // fetches data from the public folder.
-            console.log("Fetching user locale data from the web...");
-            const data = await (await fetch(`./locales/${userLocale}/currency.json`)).json();
-            CacheStorage.add("UserCurrency",`./locales/${userLocale}/currency.json`,data, 60 * 60 * 24 * 30);
-
-         } catch(error) {
-            console.log("Something went wrong.")
-            console.error(error)
-         }
-
+      ) {
+         ChangeData((PrevData) => ({
+            ...PrevData,
+            User: {
+               ...PrevData.User,
+               IsMobile: false
+            }
+         }));
       }
-
-      ChangeData((PrevData) => ({
-         ...PrevData,
-         User: {
-            ...PrevData.User,
-            BaseCurrency: data
-         }
-      }));
-
    }
 
    //
    //
-   useEffect( () => {
+   useEffect(() => {
       Promise.all([
          FetchFinancesData(),
          FetchCurenciesApiData(),
          detectMobile(),
-         DetectUserCurrency(),
          FetchValidCurrencies()
       ]);
-   },[]);
+   }, []);
 
 
-   return(<>
-      <GlobalDataContext.Provider value={{ data:data, UpdateData: ChangeData }} >
-         { children }
+   return (<>
+      <GlobalDataContext.Provider value={{ data: data, UpdateData: ChangeData }} >
+         {children}
       </GlobalDataContext.Provider>
    </>);
 };
@@ -148,9 +140,9 @@ export interface IGlobalData {
    FetchingFinanceData: boolean
    Exchange: MoneyConversionApiResponse | undefined
    FetchingExchangeData: boolean
-   ValidCurrencies: {[code: string]: Currency},
+   ValidCurrencies: { [code: string]: Currency },
    FetchingCurrencies: boolean,
-   
+
 
 }
 export type GlobalDataContextType = {
@@ -159,16 +151,16 @@ export type GlobalDataContextType = {
 }
 
 export type UserType = {
-   IsMobile : boolean
-   BaseCurrency : Currency | undefined
+   IsMobile: boolean
+   BaseCurrency: Currency | undefined
 }
 
 export type Currency = {
-   code : string,
-   name : string,
-   decimal_digits : number,
-   name_plural : string,
-   rounding : number,
-   symbol : string,
-   symbol_native : string
+   code: string,
+   name: string,
+   decimal_digits: number,
+   name_plural: string,
+   rounding: number,
+   symbol: string,
+   symbol_native: string
 }
