e825195764bebaf4b17edcc44040087aa31352d6
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-25 20:19:24 -0300

    re-organizing configuration page to handle the new components

--- src/pages/Configuration.tsx
+++ src/pages/Configuration.tsx
@@ -1,26 +1,46 @@
 import { useContext } from "react";
 import { GlobalDataContext, GlobalDataContextType } from "../Data/Contexts/GlobalDataContext";
 import ThemePicker from "../components/Configurables/ThemePicker";
+import UserSettingSwitches from "../components/Configurables/UserSettingSwitches";
+import TreeDataDumper from "../components/Configurables/TreeDataDumper";
+import PreferableCurrency from "../components/Configurables/PreferableCurrency";
 
 export default function Configuration() {
    const context = useContext(GlobalDataContext) as GlobalDataContextType;
 
-   // user preference for AmmountType (coin type)
-   //const [selectedCoinType, setSelectedCoinType] = useState<string>( "USD");
-   //const test = MoneyConversionApi.getLatestRates().then((data) => console.log(data));
    
-   //const currencyFormater = new Intl.NumberFormat(navigator.language || 'en-US', {
-   
-   return (<section className="flex flex-wrap ">
-      <div className=" m-1 border-round w-min px-3 pb-3 pt-5 surface-ground ">
-         <ThemePicker/>
+   return (<section className="flex flex-wrap flex-grow-1 ">
+      <div className=" m-1 border-round w-min flex-grow-1 surface-ground  px-2 pb-1 ">
+         <div>
+            <h2 className="border-bottom-1 border-primary-700 mb-6 pb-2">Appearences</h2>
+
+            <ThemePicker/>
+         </div>
+         
+         <div className="flex flex-wrap gap-2 ">
+         <h2 className="border-bottom-1 w-full border-primary-700 mb-1 pb-1">Preferences</h2>
+            <div className=" border-round-3xl border-1 border-primary-100 shadow-3 py-2 px-3">
+               <UserSettingSwitches />
+            </div>
+            <div>
+               <PreferableCurrency />
+            </div>
+         
+         </div>
+         
+
+         
       </div>
 
-      <footer className="flex-grow-1 w-full p-3 surface-ground border-round-xl shadow-6 m-5">
+      <footer className="flex-grow-1 p-1 w-full surface-ground border-round-xl shadow-6 m-1">
          <span>Debug Data.</span>
-         <pre>
-            {JSON.stringify(context, null, 2)}
-         </pre>
+         <TreeDataDumper />
       </footer>
    </section>);
 }
+
+
+
+
+
+
