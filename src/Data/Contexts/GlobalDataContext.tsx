fc489270e43f9163d5621a961dd6525dcfce7ab0
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-25 20:18:11 -0300

    User settings are now a separate object inside user data | added new prefered currency data fetch.

--- src/Data/Contexts/GlobalDataContext.tsx
+++ src/Data/Contexts/GlobalDataContext.tsx
@@ -6,6 +6,25 @@ import CacheStorage from "../Cache/CacheStorage";
 
 export const GlobalDataContext = createContext<GlobalDataContextType | null>(null);
 
+export const FetchLocaleCurrency = async (locale: string) => {
+   let userLocale = locale;
+   let UserCurencyData = await CacheStorage.get("UserCurrency", `./locales/${userLocale}/currency.json`) as Currency | undefined;
+
+   if (UserCurencyData == undefined) {
+      try {
+         // fetches data from the public folder.
+         console.log("Fetching user locale data from the web...");
+         UserCurencyData = await (await fetch(`./locales/${userLocale}/currency.json`)).json() as Currency;
+         await CacheStorage.add("UserCurrency", `./locales/${userLocale}/currency.json`, UserCurencyData, 60 * 60 * 24 * 30);
+
+      } catch (error) {
+         console.log("Something went wrong.")
+         console.error(error)
+      }
+   }
+
+   return UserCurencyData;
+}
 
 const GlobalDataProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
@@ -18,12 +37,15 @@ const GlobalDataProvider: React.FC<{ children: React.ReactNode }> = ({ children 
       FetchingCurrencies: true,
       User: {
          IsMobile: true,
-         BaseCurrency: {"code":"USD","name":"US Dollar","decimal_digits":2,"name_plural":"US dollars","rounding":0,"symbol":"$","symbol_native":"$"},
+         BaseCurrency: { "code": "USD", "name": "US Dollar", "decimal_digits": 2, "name_plural": "US dollars", "rounding": 0, "symbol": "$", "symbol_native": "$" },
          //
-         AutoExchange : (localStorage.getItem("AutoExchange") === null), // default: true
-         ResetInputFormOnCancel : (localStorage.getItem("ResetInputFormOnCancel") === null),  // default: true
-         ShowForms : (localStorage.getItem("ShowForms") !== null),              // default: false,
-         ShowValues : (localStorage.getItem("ShowValues") === null)               // default: true, 
+         Settings: {
+            AutoExchange: (localStorage.getItem("AutoExchange") === null),
+            ResetInputFormOnCancel: (localStorage.getItem("ResetInputFormOnCancel") === null),
+            ShowForms: (localStorage.getItem("ShowForms") !== null),
+            ShowValues: (localStorage.getItem("ShowValues") === null),
+         },
+
       }
 
    });
@@ -34,35 +56,34 @@ const GlobalDataProvider: React.FC<{ children: React.ReactNode }> = ({ children 
    // fetches user base currency.
    // also fetches exchange data from the api or cache.
    const FetchCurenciesApiData = async () => {
-      const userLocale = navigator.language || "en-US";
-      let userCurrencyData = await CacheStorage.get("UserCurrency", `./locales/${userLocale}/currency.json`) as Currency | undefined;
+      const PreferedCurrency = localStorage.getItem("PreferedCurrency") ?? null;
 
-      if (!userCurrencyData) {
-         try {
-            // fetches data from the public folder.
-            console.log("Fetching user locale data from the web...");
-            userCurrencyData = await (await fetch(`./locales/${userLocale}/currency.json`)).json() as Currency;
-            CacheStorage.add("UserCurrency", `./locales/${userLocale}/currency.json`, userCurrencyData, 60 * 60 * 24 * 30);
+      if (PreferedCurrency) {
+         let data = JSON.parse(PreferedCurrency) as Currency;
+         ChangeData((PrevData) => ({
+            ...PrevData,
+            User: {
+               ...PrevData.User,
+               BaseCurrency: data
+            }
+         }));
+      } else {
+         const userLocale = navigator.language || "en-US";
+         let UserCurrencyData = await FetchLocaleCurrency(userLocale);
+         let exchangeRates = await MoneyConversionApi.getLatestRates(( UserCurrencyData as Currency).code);
 
-         } catch (error) {
-            console.log("Something went wrong.")
-            console.error(error)
-         }
+         ChangeData((PrevData) => ({
+            ...PrevData,
+            Exchange: exchangeRates,
+            FetchingExchangeData: false,
+            User: {
+               ...PrevData.User,
+               BaseCurrency: UserCurrencyData as Currency
+            }
+         }));
       }
-
-      const data = await MoneyConversionApi.getLatestRates((userCurrencyData as Currency).code);
-
-      ChangeData((PrevData) => ({
-         ...PrevData,
-         Exchange: data,
-         FetchingExchangeData: false,
-         User: {
-            ...PrevData.User,
-            BaseCurrency: userCurrencyData as Currency
-         }
-      }));
    }
-   
+
    // fetches finances data from the database.
    const FetchFinancesData = async () => {
       try {
@@ -159,10 +180,13 @@ export type UserType = {
    IsMobile: boolean
    BaseCurrency: Currency
    //
-   AutoExchange : boolean
-   ResetInputFormOnCancel : boolean
-   ShowForms : boolean
-   ShowValues : boolean
+   Settings: {
+      AutoExchange: boolean
+      ResetInputFormOnCancel: boolean
+      ShowForms: boolean
+      ShowValues: boolean
+   }
+
 }
 
 export type Currency = {
