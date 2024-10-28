8d9e7944a07add63242a977c45aff23150210f3c
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-28 16:29:40 -0300

    Added Transaction Get.

--- src/Data/Database/Database.ts
+++ src/Data/Database/Database.ts
@@ -11,7 +11,7 @@ export class Database {
    public static readonly DatabaseName: string = "Finances";
    public static readonly DatabaseVersion: number = 2; // pre alpha2 :3
 
-   public static async getDB() {
+   private static async getDB() {
       return await openDB(Database.DatabaseName, Database.DatabaseVersion, {
          upgrade(db) {
             console.log("Updating database...")
@@ -53,19 +53,41 @@ export class Database {
       db.close();
       return true;
    }
-   public static async UpdateTransaction(targetTransaction : Transaction) {
+   public static async UpdateTransaction(originalTransaction : Transaction,targetTransaction : Transaction) {
       const db = await Database.getDB();
+      const oldKey = new Date(originalTransaction.dateTime).toDateString();
       const key = new Date(targetTransaction.dateTime).toDateString();
 
-      const existingData = await db.get(DatabaseStores.Finances, key) as Transaction[] | undefined;
-      if (existingData != undefined) {
-         await db.put(DatabaseStores.Finances, existingData.map(t => t.createdAt === targetTransaction.createdAt ? targetTransaction : t), key)
-      } else {
-         return false;
+      const oldData = await db.get(DatabaseStores.Finances, oldKey) as Transaction[] | undefined;
+      if (oldData != undefined) {
+         await db.put(DatabaseStores.Finances, oldData.filter(t => t.createdAt !== originalTransaction.createdAt), oldKey)
+      }
+
+      const newData = await db.get(DatabaseStores.Finances, key) as Transaction[] | undefined;
+      if (newData != undefined) {
+         await db.put(DatabaseStores.Finances, [...newData, targetTransaction], key)
       }
 
       db.close();
       return true;
    }
+   //
+   // Getters.
+   //
+   public static async GetTransactions() {
+      const db = await Database.getDB();
+      const keys = await db.getAllKeys(DatabaseStores.Finances);
+      let transactions : {[key :string] : Transaction[] } = {};
+
+      for (const key of keys) {
+         const data = await db.get(DatabaseStores.Finances, key);
+         if (data) {
+            transactions[key as string] = data; // Add the array of transactions under the date key
+         }
+      }
+   
+      db.close();
+      return transactions;
+    }
 
 }
