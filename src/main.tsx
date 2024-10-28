63a34b367efff7041c76cdbdfb7501fe277bb59b
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-28 17:08:46 -0300

    added context providers

--- src/main.tsx
+++ src/main.tsx
@@ -8,23 +8,33 @@ import "./styles/main.css"
 import "primeflex/primeflex.min.css"
 import "primeicons/primeicons.css"
 import GlobalDataProvider from './Data/Contexts/GlobalDataContext.tsx'
+import FinacesContextProvider from './Data/Contexts/FinancesContext.tsx'
+import UserContextProvider from './Data/Contexts/UserContext.tsx'
+import ExchangeContextProvider from './Data/Contexts/ExchangeContexts.tsx'
 //import { AvailableThemes } from './Data/Selections/AvailableThemes.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PrimeReactProvider>
-      <App/>
+      <App />
     </PrimeReactProvider>
   </StrictMode>,
 );
 
 
 export function App() {
-  
+
   //
   return (<>
-    <GlobalDataProvider>
-      <Sidebar />
-    </GlobalDataProvider>
+    <UserContextProvider>
+      <ExchangeContextProvider>
+        <FinacesContextProvider>
+          <GlobalDataProvider>
+            <Sidebar />
+          </GlobalDataProvider>
+        </FinacesContextProvider>
+      </ExchangeContextProvider>
+    </UserContextProvider>
+
   </>)
 }
