1e34e9510a970f2ecdae776f0629226388426042
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-27 23:43:35 -0300

    made home sligthly better

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,27 @@
 import InputTransaction from "../components/InputTransaction";
-import MonthlyGraph from "../components/MonthlyGraph";
 import TransactionHistory from "../components/TransactionsHistory/TransactionHistory";
 import BalanceDisplay from "../components/BalanceDisplay";
+import DataRangeResume from "../components/DataRangeResume";
 
 
 export default function Home() {
    return (<main className="relative w-full">
       
-      <nav className=" mb-3 p-2 surface-ground shadow-3 w-full ">
+      <nav className=" mb-1 p-2 surface-ground shadow-3 w-full ">
          <div className=" flex justify-content-between align-content-center align-items-center">
             <BalanceDisplay />
             <InputTransaction/>
          </div>
       </nav>
       
-      <div className="flex flex-wrap justify-content-center px-1">
-         <header className=" flex-grow-1 w-full">
-            <MonthlyGraph/>
-         </header>
+      <article className="flex flex-wrap justify-content-center px-1">
+         <section className=" flex-grow-1 p-1 w-full">
+            <DataRangeResume/>
+         </section>
          
          <footer className="text-center flex mb-4 mt-5 flex-column w-full">
             <TransactionHistory/>
          </footer>
-      </div>
+      </article>
    </main>)
 }
