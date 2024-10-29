9dbf97c34033a82fb3e51ac419fce03869f85539
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-29 19:47:00 -0300

    finalized transactionCard new logic and design

--- src/components/TransactionRelated/TransactionCard.tsx
+++ src/components/TransactionRelated/TransactionCard.tsx
@@ -11,21 +11,35 @@ import { ExchangeContext, ExchangeContextType } from "../../Data/Contexts/Exchan
 import { CurrencyContext, CurrencyContextType } from "../../Data/Contexts/CurrencyContext";
 
 
-const TransactionCard: React.FC<{ transaction: Transaction }> = (props): JSX.Element => {
+
+const TransactionCard: React.FC<{ transaction: Transaction, removeAction?: () => void }> = (props): JSX.Element => {
+
    const userConfigs = useContext(Usercontext) as UserContextType
    const exchangeData = useContext(ExchangeContext) as ExchangeContextType
    const currencies = useContext(CurrencyContext) as CurrencyContextType
 
    const [busy, setBusy] = useState(false);
-   const [transactionData, setTransactionData] = useState(props.transaction);
-   
+   const [originalTransaction] = useState(props.transaction);
+   const [transactionData, setTransactionData] = useState(originalTransaction);
+
 
    const ShowData = userConfigs.data.Settings.ShowValues;
    //
-   const backgroundColor = transactionData.amount < 0 ? "bg-red-300" : transactionData.amount > 0 ? "bg-green-300" : "bg-gray-300"
-   const borderColor = transactionData.amount < 0 ? "border-red-700" : transactionData.amount > 0 ? "border-green-700" : "border-gray-300"
-   const textColor = transactionData.amount < 0 ? "text-red-700" : transactionData.amount > 0 ? "text-green-700" : ""
-   const icon = transactionData.amount < 0 ? "pi pi-send" : transactionData.amount > 0 ? "pi  pi-dollar" : "pi pi-arrow-down"
+   const backgroundColor = transactionData.amount < 0 ? "bg-red-300" : transactionData.amount > 0 ? "bg-green-300" : "bg-gray-300";
+   const borderColor = transactionData.amount < 0 ? "border-red-700" : transactionData.amount > 0 ? "border-green-700" : "border-gray-300";
+   const textColor = transactionData.amount < 0 ? "text-red-700" : transactionData.amount > 0 ? "text-green-700" : "";
+   const icon = transactionData.amount < 0 ? "pi pi-send" : transactionData.amount > 0 ? "pi  pi-dollar" : "pi pi-arrow-down";
+
+   const baseCurrencyEquivalent = () => {
+      if (exchangeData.data) {
+         if (transactionData.ammountType.code != userConfigs.data.BaseCurrency.code) {
+            return (<span className=" surface-200 text-primary-400 border-round-3xl px-1 ">
+               ({userConfigs.data.BaseCurrency.symbol} {transactionData.amount / exchangeData.data.rates[transactionData.ammountType.code]})
+            </span>)
+         }
+         return (undefined)
+      }
+   }
 
    const inputVisibility = userConfigs.data.Settings.ShowForms ? "" : "hidden-input";
 
@@ -33,89 +47,96 @@ const TransactionCard: React.FC<{ transaction: Transaction }> = (props): JSX.Ele
 
    const HandleBlur = async () => {
       setBusy(true); //
-      
+
       //await Database.UpdateTransaction(transactionData, transactionData)
-      
+
       //const data = await Database.GetTransactions();
 
       setBusy(false);
    }
 
    const confirmAction = async () => {
-
-      const confirmation = confirm("Are you sure you want to delete this transaction?");
-
-      if (confirmation) {
-         setBusy(true)
-         await Database.RemoveTransaction(transactionData);
-         
-
-         setBusy(false)
-         confirm("Transaction deleted!")
-      }
+      setBusy(true)
+      props.removeAction && props.removeAction()
+      setBusy(false)
    }
 
-   return (<article className={`surface-ground p-1 py-2 flex w-full max-w-25rem flex-wrap  border-1 ${borderColor} shadow-3 border-round-xl relative`}>
-      <header className="flex px-1 justify-content-between align-content-center w-full gap-1 ">
-         <i className={` ${icon} ${backgroundColor} border-round-3xl text-4xl p-2 `} />
-         <section className=" flex w-full gap-1 my-auto ">
-            <input placeholder="Transaction Title" value={transactionData.title}
-               onChange={(e) => { setTransactionData(prevData => ({ ...prevData, title: e.target.value })) }}
-               onBlur={HandleBlur}
-               className={` text-xl flex-grow-1 p-2 ${inputVisibility} surface-ground`} />
+   return (<>
+      <article className={`surface-ground p-1 py-2  w-full max-w-25rem  border-1 ${borderColor} shadow-3 border-round relative`}>
+         <aside className=" flex flex-grow-1 justify-content-between ">
+
+            <aside>
+               <div className="flex w-8rem align-items-start">
+                  <i className={` ${icon} ${backgroundColor} border-round-3xl text-xl p-2 `} />
+                  <input placeholder="Transaction Title" value={transactionData.title}
+                     onChange={(e) => { setTransactionData(prevData => ({ ...prevData, title: e.target.value })) }}
+                     onBlur={HandleBlur}
+                     className={` w-6rem flex-grow-1 p-1 text-base  ${inputVisibility} surface-ground`} />
+               </div>
+               <div className="flex justify-content-start">
+                  <Calendar touchUI={userConfigs.data.IsMobile}
+                     onChange={(e) => {
+                        setBusy(true);
+                        setTransactionData(prevData => ({ ...prevData, dateTime: (e.value as Date).getTime() }));
+                        setBusy(false);
+                     }}
+                     onBlur={HandleBlur}
+                     className="hidden-input text-left " showTime
+                     value={transactionDataTime} />
+               </div>
+            </aside>
+            <section>
+            <div>
+               {ShowData ? <span className="text-right">
+                  <span className="flex align-items-center">
+                     <NumberInput placeholder="Transaction Ammount"
+                        className={`flex-grow-1 w-8rem hidden-input text-lg p-1 mr-1 text-right ${textColor} ${inputVisibility}`}
+                        value={transactionData.amount}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                           setTransactionData(prevData => ({ ...prevData, amount: parseFloat(e.target.value) }));
+                        }}
+                        onBlur={HandleBlur}
+                     />
+                     <CurrencyDropDown className={`${inputVisibility}`}
+                        loading={exchangeData.busy}
+                        options={currencies.data}
+                        value={transactionData.ammountType}
+                        onChange={(e) => {
+
+                           setTransactionData(prevData => ({
+                              ...prevData,
+                              ammountType: e.value,
+                           }));
+                        }}
+                        onBlur={HandleBlur} />
+                  </span>
+                  <div className="text-sm">
+                     {baseCurrencyEquivalent()}
+                  </div>
+               </span>
+                  : <div className="">
+                     <i className="pi pi-lock text-xl pl-2 pr-1" />
+                     <input className="flex-grow-1 p-2 text-lg hidden-input surface-ground"
+                        placeholder="************" readOnly
+                        value={"************"} />
+                  </div>
+               }
+            </div>
+
          </section>
 
-         <div>
-            <Button className="align-self-end" icon="pi pi-trash" severity="danger" disabled={busy} outlined onClick={confirmAction} />
-         </div>
-      </header>
-      <section className="flex w-full gap-1 pt-1 px-2">
-
-         {ShowData ? <>
-
-            <CurrencyDropDown className={`${inputVisibility}`}
-               loading={exchangeData.busy}
-               options={currencies.data}
-               value={transactionData.ammountType}
-               onChange={(e) => {
-                  
-                  setTransactionData(prevData => ({
-                     ...prevData,
-                     ammountType: e.value,
-                  }));
-               }}
-               onBlur={HandleBlur} />
-
-            <NumberInput placeholder="Transaction Ammount"
-               className={`flex-grow-1 hidden-input text-lg p-2 ${textColor} ${inputVisibility}`}
-               value={transactionData.amount}
-               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setTransactionData(prevData => ({ ...prevData, amount: parseFloat(e.target.value) }));
-               }}
+         </aside>
+         
+         <footer className="flex w-full gap-2 px-1 mb-1">
+            <textarea className=" flex-grow-1 border-none p-1 border-round no-resize surface-100 "  placeholder="Category"
+               onChange={(e) => { setTransactionData(prevData => ({ ...prevData, category: e.target.value })) }}
                onBlur={HandleBlur}
-            />
-         </> : <div className="">
-            <i className="pi pi-lock text-xl pl-2 pr-1" />
-            <input className="flex-grow-1 p-2 text-lg hidden-input surface-ground"
-               placeholder="************" readOnly
-               value={"************"} />
-
-         </div>}
-
-
-
-      </section>
-      <footer className="flex w-full justify-content-end">
-         <Calendar touchUI={userConfigs.data.IsMobile}
-            onChange={(e) => {
-               setBusy(true);
-               setTransactionData(prevData => ({ ...prevData, dateTime: (e.value as Date).getTime() }));
-               setBusy(false);
-            }}
-            onBlur={HandleBlur}
-            className="hidden-input mt-2" showTime
-            value={transactionDataTime} />
-      </footer>
-   </article>)
+               value={transactionData.category}>
+            </textarea>
+            <Button onClick={confirmAction} className="flex-grow-0 " outlined icon="pi pi-trash" severity="danger" />
+               
+         </footer>
+      </article>
+   </>);
 };
 export default TransactionCard
