83ed6cdf300f39ef723d588daf80f2f155707b23
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-29 19:46:47 -0300

    split home to better handle the new logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,39 @@
-import InputTransaction from "../components/InputTransaction";
-import TransactionHistory from "../components/TransactionsHistory/TransactionHistory";
+import InputTransaction from "../components/TransactionRelated/InputTransaction";
+import TransactionHistory from "../components/TransactionRelated/TransactionHistory";
 import BalanceDisplay from "../components/BalanceDisplay";
 import DataRangeResume from "../components/DataRangeResume";
 
 
 export default function Home() {
+
    return (<main className="relative w-full">
-      
       <nav className=" mb-1 p-2 surface-ground shadow-3 w-full ">
          <div className=" flex justify-content-between align-content-center align-items-center">
             <BalanceDisplay />
-            <InputTransaction/>
+            <InputTransaction />
          </div>
       </nav>
-      
+
+
       <article className="flex flex-wrap justify-content-center px-1">
-         <section className=" flex-grow-1 p-1 w-full">
-            <DataRangeResume/>
-         </section>
-         
+
+
          <footer className="text-center flex mb-4 mt-5 flex-column w-full">
-            <TransactionHistory/>
+            <TransactionHistory />
          </footer>
       </article>
+
+   </main>)
+
+
+   return (<main className="relative w-full">
+
+      
+         <section className=" flex-grow-1 p-1 w-full">
+            <DataRangeResume />
+         </section>
+
+         
+      
    </main>)
 }
