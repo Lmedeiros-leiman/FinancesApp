0c2e3eabdd34adf96b28c8ece2957bf012b6c88e
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-29 17:15:29 -0300

    fixed caching

--- src/Data/WebFetcher/CachedFetcher.ts
+++ src/Data/WebFetcher/CachedFetcher.ts
@@ -1,34 +1,42 @@
 
-export const JsonFetcher = async <T = any>(url: string, cachedTime : number = 1 * 1000 ) => {
+export const JsonFetcher = async <T> (url: string, cachedTime : number = 1 * 1000 ) => {
    // cached time is measured in seconds.
    // first we check the cache for our request.
    const cache = await caches.open("CachedJsonRequests");
-   let CachedData = await cache.match(url);
-   
-   if (CachedData) {
-      const isStale = Number(CachedData.headers.get("x-cached-time")) < new Date().getTime();
+   const cachedResponse = await cache.match(url);
 
-      if (isStale) { 
+   if (cachedResponse) {
+      const cachedTimeHeader = Number(cachedResponse.headers.get("x-cached-time"));
+      const isStale = new Date().getTime() > cachedTimeHeader;
+      console.log("Cache TIme: "+ cachedTimeHeader + "| Current Time: " + new Date().getTime() + "| Stale: " + isStale)
+      if (isStale) {
+         // Cache is stale; delete it
+         console.log("Deleting cached data for", url);
          await cache.delete(url);
       } else {
-         console.log("Using cached data for " + url);
-         return await CachedData.json() as T;
+         console.log(`Using cached data for ${url}`);
+         return await cachedResponse.json() as T;
+         
       }
+      
    }
 
-   const header = await fetch(url);
-   const data = await header.json() as T;
+   // Fetch new data as cache was missing or stale
+   console.log(`Fetching new data for ${url}`);
+   const response = await fetch(url);
+   
 
-   // make sure to only cache sucessfull responses!
-   if (header.ok) {
-      const newHeader = new Headers(header.headers)
-      newHeader.append("x-cached-time", String(new Date().getTime() + cachedTime));
+   if (response.ok) {
+      const data = await response.json() as T;
+      const headers = new Headers(response.headers);
+      headers.append("x-cached-time", String(new Date().getTime() + cachedTime * 1000));
+      console.log("Caching data for ", url);
+      await cache.put(url, new Response(JSON.stringify(data), { headers }));
 
-      await cache.put(url, new Response(JSON.stringify(data), { headers: newHeader }) );
+      return data;
    }
-   
-   
-   return data as T;
+
+   return response;
 }
 
 
