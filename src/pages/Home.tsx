d47e766cd25cf64da15c1c99e62afc9d260790a3
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-10-31 21:02:38 -0300

    Removed Home navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,17 @@
-import InputTransaction from "../components/TransactionRelated/InputTransaction";
 import TransactionHistory from "../components/TransactionRelated/TransactionHistory";
-import BalanceDisplay from "../components/BalanceDisplay";
-import DataRangeResume from "../components/DataRangeResume";
+import DataRangeResume from "../components/DataRangeResume/DataRangeResume";
 
 
 export default function Home() {
 
    return (<main className="relative w-full">
-      <nav className=" mb-1 p-2 surface-ground shadow-3 w-full ">
-         <div className=" flex justify-content-between align-content-center align-items-center">
-            <BalanceDisplay />
-            <InputTransaction />
-         </div>
-      </nav>
-
-
-      <article className="flex flex-wrap justify-content-center px-1">
+      <article className="flex flex-wrap justify-content-center px-1 mt-1">
 
          <section className=" flex flex-wrap gap-1 flex-grow-1 p-1 w-full surface-ground border-1 border-gray-400 p-1 border-round">
             <DataRangeResume />
          </section>
 
-         <footer className="text-center flex mb-4 mt-5 flex-column w-full">
+         <footer className="text-center flex mb-4 mt-5 flex-column w-full flex-grow-1">
             <TransactionHistory />
          </footer>
       </article>
